aca7b8832784f0433d96078620911baabb1948fd
Gino-Y <ginoyan250@gmail.com>
2021-09-17 23:45:00 +0800

    页面布局练习

--- src/pages/gino/index.js
+++ src/pages/gino/index.js
@@ -6,13 +6,30 @@ const Gino = () => {
     return (
         <div className={styles.global}>
             <div className={styles.firstLine}>
-                <div className={styles.first}></div>
-                <div className={styles.second}></div>
-                <div className={styles.third}></div>
-                <div className={styles.clear}></div>
+                <div className={styles.firstOne}></div>
+                <div className={styles.secondOne}></div>
+                <div className={styles.thirdOne}></div>
+                <div classname={styles.clear}></div>
             </div>
-            <div className={styles.secondLine}></div>
-            <div className={styles.clear}></div>
+            <div className={styles.secondLine}>
+                <div className={styles.firstTow}>
+                    <div className={styles.firstA}>
+                        <div className={styles.firstAa}></div>
+                        <div className={styles.firstBa}></div>
+                        <div classname={styles.clear}></div>
+                    </div>
+                    <div className={styles.firstB}></div>
+                    <div classname={styles.clear}></div>
+                </div>
+                <div className={styles.secondTow}></div>
+                <div classname={styles.clear}></div>
+            </div>
+            <div className={styles.thirdLine}>
+                <div className={styles.firstThree}></div>
+                <div className={styles.secondThree}></div>
+                <div classname={styles.clear}></div>
+            </div>
+            <div classname={styles.clear}></div>
         </div>
     );
 };
