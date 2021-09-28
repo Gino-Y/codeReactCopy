1bd6da94a78911592e1954b3da4a4ab3685fa9d5
Gino-Y <ginoyan250@gmail.com>
2021-09-29 00:22:38 +0800

    项目计划页面布局

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -38,7 +38,7 @@ const Blog = () => {
                     </div>
                     <div className={styles.blogBox}>
                         <div className={styles.imgBox}>
-                            <img src={girlxx01} className={styles.girlxx01} />
+                            <img src={girlxx01} className={styles.girlImg} />
                         </div>
                         <div className={styles.content}>
                             <div className={styles.introduction}>
@@ -63,7 +63,7 @@ const Blog = () => {
                     </div>
                     <div className={styles.blogBox}>
                         <div className={styles.imgBox}>
-                            <img src={girlxx02} className={styles.girlxx01} />
+                            <img src={girlxx02} className={styles.girlImg} />
                         </div>
                         <div className={styles.content}>
                             <div className={styles.introduction}>
@@ -88,7 +88,7 @@ const Blog = () => {
                     </div>
                     <div className={styles.blogBox}>
                         <div className={styles.imgBox}>
-                            <img src={girlxx03} className={styles.girlxx01} />
+                            <img src={girlxx03} className={styles.girlImg} />
                         </div>
                         <div className={styles.content}>
                             <div className={styles.introduction}>
@@ -115,18 +115,127 @@ const Blog = () => {
                 </div>
                 <div className={styles.mainBox}>
                     <div className={styles.blogBoxT2}>
-                        <img src={girlxx04} className={styles.girlxx01} />
+                        <div className={styles.imgBox}>
+                            <img src={girlxx04} className={styles.girlImg} />
+                        </div>
+                        <div className={styles.content}>
+                            <div className={styles.introduction}>
+                                令人鼓舞的动感体验！发自内心的飘逸~
+                                <br />
+                                平面设计
+                            </div>
+                            <div className={styles.introductionOne}>
+                                When it comes to parallax scrolling,the that
+                                separates good and bad choices is pretty thin. A
+                                while back parallax was being used to deliver an
+                                explosion of ilimints floating around the
+                                screen.
+                            </div>
+                        </div>
+                        <div className={styles.like}>❤ 15</div>
+                        <div className={styles.info}>
+                            by John Doe / 4 comments / Oct. 12. 2013
+                        </div>
+                        <div className={styles.clear}></div>
                     </div>
                     <div className={styles.blogBox}>
-                        <img src={girlxx05} className={styles.girlxx01} />
+                        <div className={styles.imgBox}>
+                            <img src={girlxx05} className={styles.girlImg} />
+                        </div>
+                        <div className={styles.content}>
+                            <div className={styles.introduction}>
+                                令人鼓舞的动感体验！发自内心的飘逸~
+                                <br />
+                                平面设计
+                            </div>
+                            <div className={styles.introductionOne}>
+                                When it comes to parallax scrolling,the that
+                                separates good and bad choices is pretty thin. A
+                                while back parallax was being used to deliver an
+                                explosion of ilimints floating around the
+                                screen.
+                            </div>
+                        </div>
+                        <div className={styles.like}>❤ 15</div>
+                        <div className={styles.info}>
+                            by John Doe / 4 comments / Oct. 12. 2013
+                        </div>
+                        <div className={styles.clear}></div>
+                        <div className={styles.clear}></div>
                     </div>
                     <div className={styles.blogBox}>
-                        <img src={girlxx06} className={styles.girlxx01} />
+                        <div className={styles.imgBox}>
+                            <img src={girlxx06} className={styles.girlImg} />
+                        </div>
+                        <div className={styles.content}>
+                            <div className={styles.introduction}>
+                                令人鼓舞的动感体验！发自内心的飘逸~
+                                <br />
+                                平面设计
+                            </div>
+                            <div className={styles.introductionOne}>
+                                When it comes to parallax scrolling,the that
+                                separates good and bad choices is pretty thin. A
+                                while back parallax was being used to deliver an
+                                explosion of ilimints floating around the
+                                screen.
+                            </div>
+                        </div>
+                        <div className={styles.like}>❤ 15</div>
+                        <div className={styles.info}>
+                            by John Doe / 4 comments / Oct. 12. 2013
+                        </div>
+                        <div className={styles.clear}></div>
+                        <div className={styles.clear}></div>
                     </div>
                     <div className={styles.clear}></div>
                 </div>
                 <div className={styles.search}></div>
-                <div className={styles.suggest}></div>
+                <div className={styles.suggest}>
+                    <div className={styles.tag}>项目计划</div>
+                    <div className={styles.tagContent}>
+                        <div className={styles.tagIcon}>></div>
+                        <div className={styles.tagTex}>2016年11月22号</div>
+                    </div>
+                    <div className={styles.tagContent}>
+                        <div className={styles.tagIcon}>></div>
+                        <div className={styles.tagTex}>
+                            SKOKOV制作的顺应企业的主题
+                        </div>
+                    </div>
+                    <div className={styles.tagContent}>
+                        <div className={styles.tagIcon}>></div>
+                        <div className={styles.tagTex}>网页设计</div>
+                    </div>
+                    <div className={styles.tagContent}>
+                        <div className={styles.tagIcon}>></div>
+                        <div className={styles.tagTex}>
+                            IE浏览器、火狐浏览器、谷歌浏览器
+                        </div>
+                    </div>
+                    <div className={styles.tagContent}>
+                        <div className={styles.tagIcon}>></div>
+                        <div className={styles.tagTex}>
+                            WORDPRESS 3.5、WORDPRESS 3.6
+                        </div>
+                    </div>
+                    <div className={styles.tagContent}>
+                        <div className={styles.tagIcon}>></div>
+                        <div className={styles.tagTex}>客户效应</div>
+                    </div>
+                    <div className={styles.tagContent}>
+                        <div className={styles.tagIcon}>></div>
+                        <div className={styles.tagTex}>商标效应</div>
+                    </div>
+                    <div className={styles.tagContent}>
+                        <div className={styles.tagIcon}>></div>
+                        <div className={styles.tagTex}>独特的ICONS</div>
+                    </div>
+                    <div className={styles.tagContent}>
+                        <div className={styles.tagIcon}>></div>
+                        <div className={styles.tagTex}>预期目标</div>
+                    </div>
+                </div>
                 <div className={styles.suggest}></div>
                 <div className={styles.label}></div>
                 <div className={styles.clear}></div>
