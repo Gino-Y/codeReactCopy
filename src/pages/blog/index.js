ae5eeb946228d07fa5375ca51fd985394428bcbf
Gino-Y <ginoyan250@gmail.com>
2021-09-30 16:24:45 +0800

    项目计划页面布局

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -236,8 +236,62 @@ const Blog = () => {
                         <div className={styles.tagTex}>预期目标</div>
                     </div>
                 </div>
-                <div className={styles.suggest}></div>
-                <div className={styles.label}></div>
+                <div className={styles.suggest}>
+                    <div className={styles.tag}>受欢迎的项目案例</div>
+                    <div className={styles.projectBrief}>
+                        <div className={styles.tinyImg}>
+                            <img src={girlxx04} className={styles.tinyImg} />
+                        </div>
+                        <div className={styles.tinyTag}>
+                            拉力三又开始了，看看我们准备了什么？网页设计
+                        </div>
+                        <div className={styles.tinyDate}>Oct.10.2013</div>
+                        <div className={styles.tinyLike}>❤21</div>
+                    </div>
+                    <div className={styles.projectBrief}>
+                        <div className={styles.tinyImg}>
+                            <img src={girlxx02} className={styles.tinyImg} />
+                        </div>
+                        <div className={styles.tinyTag}>
+                            拉力三又开始了，看看我们准备了什么？网页设计
+                        </div>
+                        <div className={styles.tinyDate}>Oct.10.2013</div>
+                        <div className={styles.tinyLike}>❤21</div>
+                    </div>
+                    <div className={styles.projectBrief}>
+                        <div className={styles.tinyImg}>
+                            <img src={girlxx03} className={styles.tinyImg} />
+                        </div>
+                        <div className={styles.tinyTag}>
+                            拉力三又开始了，看看我们准备了什么？网页设计
+                        </div>
+                        <div className={styles.tinyDate}>Oct.10.2013</div>
+                        <div className={styles.tinyLike}>❤21</div>
+                    </div>
+                    <div className={styles.projectBrief}>
+                        <div className={styles.tinyImg}>
+                            <img src={girlxx04} className={styles.tinyImg} />
+                        </div>
+                        <div className={styles.tinyTag}>
+                            拉力三又开始了，看看我们准备了什么？网页设计
+                        </div>
+                        <div className={styles.tinyDate}>Oct.10.2013</div>
+                        <div className={styles.tinyLike}>❤21</div>
+                    </div>
+                    <div className={styles.projectBrief}>
+                        <div className={styles.tinyImg}>
+                            <img src={girlxx05} className={styles.tinyImg} />
+                        </div>
+                        <div className={styles.tinyTag}>
+                            拉力三又开始了，看看我们准备了什么？网页设计
+                        </div>
+                        <div className={styles.tinyDate}>Oct.10.2013</div>
+                        <div className={styles.tinyLike}>❤21</div>
+                    </div>
+                </div>
+                <div className={styles.label}>
+                    <div className={styles.tag}>标签</div>
+                </div>
                 <div className={styles.clear}></div>
             </div>
             <div classname={styles.clear}></div>
