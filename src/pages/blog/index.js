9ac62d2c24cd9422934c6f412b7d870e690aacb0
Gino-Y <ginoyan250@gmail.com>
2021-10-16 17:27:33 +0800

    新页面blog

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -16,8 +16,14 @@ const Blog = () => {
     return (
         <div className={styles.global}>
             <div className={styles.firstLine}></div>
-
             <div className={styles.secondLine}></div>
+            <div className={styles.thirdLine}></div>
+            <div className={styles.fourthLine}></div>
+            <div className={styles.fifthLine}></div>
+            <div className={styles.sixthLine}></div>
+            <div className={styles.seventhLine}></div>
+            <div className={styles.Other}></div>
+            <div className={styles.endLine}></div>
         </div>
     );
 };
